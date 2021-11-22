d2be8f8c51e8a954b23b5629f52f79308e7ae23e
Tonia <tonia.saba@gmail.com>
2021-11-21 22:59:28 -0500

    Able to input year as string and turn it into a datetime object

--- src/javaGui/BirthdayGUI.java
+++ src/javaGui/BirthdayGUI.java
@@ -11,7 +11,11 @@ import java.awt.GridLayout;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.time.LocalDate;
+import java.time.Period;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class BirthdayGUI implements ActionListener {
@@ -29,6 +33,10 @@ public class BirthdayGUI implements ActionListener {
 		
 //		Panel
 		panel = new JPanel();
+
+//		Show input example to user	
+		JLabel dateExample = new JLabel();
+		dateExample.setText("<html><p>Date input example: dd/MM/yyyy </p></html>");
 		
 //		textField
 		textField = new JTextField(20);
@@ -42,6 +50,7 @@ public class BirthdayGUI implements ActionListener {
 		
 		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10,30));
 		panel.setLayout(new GridLayout(0,1));
+		panel.add(dateExample);
 		panel.add(textField);
 		panel.add(button); // adds button to panel	
 		panel.add(label); // adds label to panel
@@ -62,12 +71,32 @@ public class BirthdayGUI implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		label.setText(textField.getText());
-		
-		String date = label.getText();
-		
-		label.setText("Your Age is: " + date);
-		
+		String action = e.getActionCommand();
+		if (action.equals("Calculate Age")) {
+			
+//			retrieves text inputed into textField
+			label.setText(textField.getText());
+			
+//			assigns text to textBday
+			String textBday = label.getText();
+			
+//			parses text date to an actual date
+			Date birthDate = null;
+			try {
+				birthDate = new SimpleDateFormat("dd/MM/yyyy").parse(textBday);
+			} catch (ParseException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+//			assign variable to today's date
+			LocalDate today = LocalDate.now();
+			
+//			Calculates period between birthDate and today
+			
+			label.setText("Your Age is: " + birthDate + "\n" + today);
+			
+		}
 	}
 
 }
